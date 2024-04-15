01732ca74c527cd2bb604b7d7460daa73937f2c3
MatteoDiMartino <matteodimartino359@gmail.com>
2024-04-16 00:20:01 +0200

    feat: add test element

--- src/components/Cardlist.vue
+++ src/components/Cardlist.vue
@@ -8,6 +8,14 @@ export default {
 
 <template>
 
-ciao
+<div class="col bg-white my-3">
+    <img src="https://qph.cf2.quoracdn.net/main-qimg-98732885e1731509a4ce78d853dd0173-pjlq" alt="img" class="ms-w-card">
+    <h1>ciao</h1>
+</div>
 
 </template>
+
+<style scoped lang="scss">
+
+
+</style>
