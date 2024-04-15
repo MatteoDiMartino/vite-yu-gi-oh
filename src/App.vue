514063f559742ea36e8bb9bdd8aebaa28d3ca50b
MatteoDiMartino <matteodimartino259@gmail.com>
2024-04-15 15:56:05 +0200

    feat: add base

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+  import AppHeader from './components/AppHeader.vue';
+
+  export default {
+    components: {
+      AppHeader
+    }
+  }
+</script>
+
+<template>
+
+<AppHeader></AppHeader>
+
+</template>
+
+<style scoped lang="scss">
+ @use './style/generic.scss'
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<script>
+
+    export default {
+        name: 'AppHeader'
+    }
+
+</script>
+
+<template>
+
+    <h1>ciaao</h1>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
