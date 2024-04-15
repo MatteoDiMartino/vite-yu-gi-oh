dd5d576c5bd96658f59cce1c23c059c75030b158
MatteoDiMartino <matteodimartino359@gmail.com>
2024-04-16 01:08:36 +0200

    feat: add axios function and API url

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,10 @@
     },
     methods: {
       getCardFromApi() {
+        axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+        .then((response) => {
 
+        })
       }
     },
     mounted() {
