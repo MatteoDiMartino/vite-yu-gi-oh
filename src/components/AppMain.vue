8c2bfb1bc9f7e7e1ea08509ebbe8da771257d402
MatteoDiMartino <matteodimartino359@gmail.com>
2024-04-16 00:10:00 +0200

    feat: add bg and vh height

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,5 +5,23 @@
 </script>
 
 <template>
-    <h1>ciao</h1>
+    <main class="ms-main-bg">
+        <div class="container">
+            <div class="row">
+                <div class="ms-h"></div>
+            </div>
+        </div>
+    </main>
 </template>
+
+<style scoped lang="scss">
+
+.ms-main-bg {
+    background-color: #D48F38;
+
+    .ms-h {
+        height: 88.9vh;
+    }
+}
+
+</style>
