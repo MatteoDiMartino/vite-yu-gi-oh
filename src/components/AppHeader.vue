2a28063fd781d5bede240e57225d50d8dbe7917c
MatteoDiMartino <matteodimartino359@gmail.com>
2024-04-15 22:39:09 +0200

    feat: add base element header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,6 +8,16 @@
 
 <template>
 
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <img src="../assets/img/yugiohlogo.bmp" alt="logo" class="w-25">
+                <span>Yu-Gi-Oh</span>
+            </div>
+        </div>
+    </div>
+</header>
 
 </template>
 
