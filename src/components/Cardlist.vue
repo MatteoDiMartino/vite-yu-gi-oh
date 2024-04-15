90254b827e58b6f298d467e9e69ed05f52f25d88
MatteoDiMartino <matteodimartino359@gmail.com>
2024-04-16 00:22:04 +0200

    feat: add flex div

--- src/components/Cardlist.vue
+++ src/components/Cardlist.vue
@@ -8,10 +8,13 @@ export default {
 
 <template>
 
-<div class="col bg-white my-3">
-    <img src="https://qph.cf2.quoracdn.net/main-qimg-98732885e1731509a4ce78d853dd0173-pjlq" alt="img" class="ms-w-card">
-    <h1>ciao</h1>
+<div class="d-flex">
+    <div class="my-3">
+        <img src="https://qph.cf2.quoracdn.net/main-qimg-98732885e1731509a4ce78d853dd0173-pjlq" alt="img" class="ms-w-card">
+        <h1>ciao</h1>
+    </div>
 </div>
+
 
 </template>
 
