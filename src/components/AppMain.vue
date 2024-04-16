461f18a1b7094930fcdca95743e560118cbfd782
MatteoDiMartino <matteodimartino359@gmail.com>
2024-04-16 14:20:21 +0200

    remove txt

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -19,7 +19,7 @@
 <template>
     <main class="ms-main-bg">
         <div class="container">
-                <div class="bg-black text-white px-3 py-3">ciao</div>
+                <div class="bg-black text-white px-3 py-3"></div>
                 <div class="ms-h d-flex justify-content-center bg-white">
                     <Cardlist v-for="mscard in store.cards" :key="mscard.id" :cardInfo="mscard" class="mt-4"></Cardlist>
                 </div>
