409b50dcf29303a82d19b3523e6a7a99536a73da
MatteoDiMartino <matteodimartino359@gmail.com>
2024-04-16 14:19:23 +0200

    feat: add black box

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -18,7 +18,8 @@
 
 <template>
     <main class="ms-main-bg">
-        <div class="container ">
+        <div class="container">
+                <div class="bg-black text-white px-3 py-3">ciao</div>
                 <div class="ms-h d-flex justify-content-center bg-white">
                     <Cardlist v-for="mscard in store.cards" :key="mscard.id" :cardInfo="mscard" class="mt-4"></Cardlist>
                 </div>
